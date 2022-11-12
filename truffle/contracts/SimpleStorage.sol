b3fd07936c5166e35158dd79f7f1bca916059629
Shubham Soni <sandhyayamlur1@gmail.com>
2022-11-12 14:17:31 +0530

    Contract/Cloud && SimpleStorage code

--- truffle/contracts/SimpleStorage.sol
+++ truffle/contracts/SimpleStorage.sol
@@ -2,13 +2,19 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract SimpleStorage {
-  uint256 value;
+  address public owner;
+  uint public last_completed_migration;
 
-  function read() public view returns (uint256) {
-    return value;
+  modifier restricted() {
+    if (msg.sender == owner) _;
+
   }
 
-  function write(uint256 newValue) public {
-    value = newValue;
+  constructor(){
+    owner = msg.sender;
+  }
+
+  function setCompleted(uint completed) public restricted {
+    last_completed_migration = completed;
   }
 }
